df6c5ab3ae517ea377398bfdf98ed3d52ce44dba
PTLab84 <protraininglab84@gmail.com>
2025-08-22 15:48:11 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-
-import SmartBooking from '../components/SmartBooking';
+import SmartBooking from './components/SmartBooking';
 
 export default function Page() {
   return <SmartBooking />;
